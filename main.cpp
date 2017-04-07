859c0da504ac153d23480a58a939e8a6e6b7c65f
Chaima El Asmi <chaima.elasmi@outlook.com>
2017-04-07 16:54:34 -0400

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,80 +17,42 @@ double horloge() {
 
 int main(int argc, char *argv[]) {
 
-        int nbImages = 400;
-        Mat img[nbImages];
+    int nbImages = 100;
+    Mat img[nbImages];
 
-       // Mat resized[nbImages];
+    VideoCapture cap(1);
+    if(!cap.isOpened())
+        return -1;
 
+    for(int i = 0; i < 30; i++)
+        cap >> img[0]; //Démarrer la caméra
 
-        //Plusieurs tests
-        //for (int j = 0; j < 4; j++) {
+    cout << "Camera ready" << endl;
 
-            VideoCapture cap(1);
+    srand(time(NULL));
+    int random = rand() % 500000;
+    cout << "Camera wait (random) : " << random << endl;
+    usleep(random);
 
-            for(int i = 0; i < 30; i++) {
-                cap >> img[0]; //Démarrer la caméra
-            }
-            cout << "Pres" << endl;
+    double timeS = horloge();
+    //Capture
+    for(int i = 0; i < nbImages; i++)
+        cap >> img[i];
 
-            waitKey(0);
+    double timeE = horloge();
+    cout << "Time (30 images) : " << ((timeE - timeS) / nbImages) * 30 << endl;
 
-            if(!cap.isOpened())
-                return -1;
+    namedWindow("Display Image", 1);
 
+    //Save and Show
+    for(int i = 0; i < nbImages; i++) {
+        imwrite( format("/home/chaima/Documents/Workspace/CaptureVid2/Images/testGit/image_%04d.jpg",
+        i), img[i] );
 
-            //Video vlc monitor screen
-            //system("vlc --qt-fullscreen-screennumber=1 -f '/home/chaima/Documents/Mathematica/Video/sequence5_32.mp4' "
-              //     "--play-and-exit --no-video-title-show --aspect-ratio 4:3 &");
+        imshow("Display Image", img[i]);
 
-            //Video vlc default screen
-            // system("vlc '/home/chaima/Documents/Mathematica/Video/sequence3.mp4' "
-               //     "-f --play-and-exit --no-video-title-show &");
+        waitKey(30);
+    }
 
-            //Vidéo mplayer
-            //system("mplayer '/home/chaima/Documents/Mathematica/Video/sequence3.mp4' "
-              //     "-geometry 800x600+1366+0 -noborder -aspect 4:3 &");
-
-
-            srand(time(NULL));
-            int random = rand() % 500000;
-            cout << "random : " << random << endl;
-            usleep(random);
-
-
-            double timeS = horloge();
-            for(int i = 0; i < nbImages; i++) {
-
-                cap >> img[i];
-                //resize(img[i], resized[i], Size(640,480)); //(683,384)
-            }
-
-            double timeE = horloge();
-
-            cout << "Time : " << (timeE - timeS) / nbImages << endl;
-            cout << "Time (" << nbImages << " images): " << (timeE - timeS) << endl;
-
-            waitKey(0);
-
-            namedWindow("Display Image", 1);
-            //Changer la fenetre d'affichage
-            /*resizeWindow("Display Image", 800, 600);
-            moveWindow("Display Image", 1366, 0);*/
-
-            for(int i = 0; i < nbImages; i++) {
-
-                //imwrite( format("/home/chaima/Documents/Workspace/CaptureVid2/Images/Seq4/tests/test%d"
-                              //  "/image_%04d.jpg", (j + 1), i) , img[i] );
-
-                imwrite( format("/home/chaima/Documents/Workspace/CaptureVid2/Images/Seq6/test7/image_%04d.jpg",
-                                  i), img[i] );
-
-                imshow("Display Image", img[i]);
-
-                waitKey(30);
-
-            }
-        //}
-
-        return 0;
+    return 0;
 }
